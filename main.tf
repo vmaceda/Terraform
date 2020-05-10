12e394ce4282c9f2caf07dbb8857584a2f816d88
vmaceda <vmaceda@hotmail.com>
2020-05-10 18:09:19 +1200

    added virtual network

--- main.tf
+++ main.tf
@@ -11,3 +11,11 @@ resource "azurerm_resource_group" "rg" {
     Department    = "Technology"
   }
 }
+
+resource "azurerm_virtual_network" "vnet" {
+  name = "myTFVNET"
+  address_space = ["10.0.0.0/16"]
+  location = "westus2"
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
